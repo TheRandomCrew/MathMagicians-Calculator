7741c87077f47fc377f66a4208f4dad43c55886c
Israel-Laguan <israellaguan@gmail.com>
2020-03-05 22:30:30 -0500

    [feat] Add calculate to App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import css from '../style/App.module.css';
+import calculate from '../logic/calculate';
+
+const defaultState = { total: '0', operation: null, next: null };
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class App extends Component {
+  constructor() {
+    super();
+    this.calculation = calculate(defaultState);
+  }
+
   render() {
     return (
       <div data-testid="app" className={css.container}>
