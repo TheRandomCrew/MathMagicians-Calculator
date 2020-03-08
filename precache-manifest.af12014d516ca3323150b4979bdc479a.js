1cd111bb1c879a3e878c2255a259d1d6e5f8b9e9
Israel-Laguan <israellaguan@gmail.com>
2020-03-08 10:54:30 -0500

    [eat] production build
    What?:
    
    Breaking Changes?

---
+++ precache-manifest.af12014d516ca3323150b4979bdc479a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7705ffd62e6a5a48dc025cfda6d12b03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "383c5fd43b2b8c33ce7e",
+    "url": "/static/css/main.16a0adc9.chunk.css"
+  },
+  {
+    "revision": "ee22e7888adc864238d1",
+    "url": "/static/js/2.120cf208.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.120cf208.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "383c5fd43b2b8c33ce7e",
+    "url": "/static/js/main.1dfaa8db.chunk.js"
+  },
+  {
+    "revision": "b31aabde7ad8eef54860",
+    "url": "/static/js/runtime-main.e98aeda3.js"
+  }
+]);
